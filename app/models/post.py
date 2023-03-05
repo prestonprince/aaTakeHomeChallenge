5faf0e5656894b9e285ab7a89e636f50f8dd983f
preston <prestonprince18@gmail.com>
2023-03-04 19:48:26 -0700

    added post table

---
+++ app/models/post.py
@@ -0,0 +1,13 @@
+from .db import db
+from sqlalchemy.sql import func
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    coffee_id = db.Column(db.Integer, db.ForeignKey('coffee.id'), nullable=False)
+    text = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Numeric(precision=2, scale=1), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
